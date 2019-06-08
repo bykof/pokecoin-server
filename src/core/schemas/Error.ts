66b7d903c6b6bdd93d7e031676e311f019bdbdb4
Michael Bykovski <michael.bykovskii@aoe.com>
2019-06-09 00:24:47 +0200

    Add Error Schema

---
+++ src/core/schemas/Error.ts
@@ -0,0 +1,9 @@
+export default {
+  type: 'object',
+  properties: {
+    // TODO: Make codes visible in the doc
+    code: { type: 'string' },
+    message: { type: 'string' },
+    payload: { type: 'object' }
+  }
+}
