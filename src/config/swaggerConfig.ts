6fd3f5b1df9764baff3184d25d1860adf47caaa8
Michael Bykovski <michael.bykovskii@aoe.com>
2019-08-08 23:58:10 +0200

    Add https to schemes

--- src/config/swaggerConfig.ts
+++ src/config/swaggerConfig.ts
@@ -23,6 +23,7 @@ const swaggerConfig = {
 if (!process.env.PRODUCTION) {
   swaggerConfig.swagger['host'] = '0.0.0.0:3000'
 } else {
+  swaggerConfig.swagger['schemes'] = ['https']
   swaggerConfig.swagger['host'] = 'rocky-lowlands-35145.herokuapp.com'
 }
 
