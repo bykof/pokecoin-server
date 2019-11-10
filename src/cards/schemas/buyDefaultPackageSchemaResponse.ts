9835430993e6f3c83276e602ae42167848600ddf
Michael Bykovski <michael.bykovskii@aoe.com>
2019-11-10 19:57:25 +0100

    Add description to buyDefaultPackageSchemaResponse

--- src/cards/schemas/buyDefaultPackageSchemaResponse.ts
+++ src/cards/schemas/buyDefaultPackageSchemaResponse.ts
@@ -1,6 +1,7 @@
 export default {
   $id: 'BuyDefaultPackageSchemaResponse',
   title: 'BuyDefaultPackageSchemaResponse',
+  description: 'Buy a default cardpackage and receive an array of bought cards',
   type: 'object',
   properties: {
     cards: { type: 'array', items: 'PokemonCard#'}
