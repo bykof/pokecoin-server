7b9d2931001d55f891cd16bd9e9142646028b4f6
Michael Bykovski <michael.bykovskii@aoe.com>
2019-11-10 19:58:03 +0100

    Update dist

--- dist/src/cards/schemas/buyDefaultPackageSchemaResponse.js
+++ dist/src/cards/schemas/buyDefaultPackageSchemaResponse.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     $id: 'BuyDefaultPackageSchemaResponse',
     title: 'BuyDefaultPackageSchemaResponse',
+    description: 'Buy a default cardpackage and receive an array of bought cards',
     type: 'object',
     properties: {
         cards: { type: 'array', items: 'PokemonCard#' }
