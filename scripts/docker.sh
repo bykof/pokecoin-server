be52770ad3eb0837c63af6b757287209202df5ea
Michael Bykovski <bykof@me.com>
2022-11-15 11:33:06 +0100

    Adapt docker buildx for multiple platforms

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -3,8 +3,4 @@ set -ex
 REPO=bykof/pokecoin-server
 GIT_TAG=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))
 
-docker build . -t $REPO:latest -t $REPO:$GIT_TAG
-
-for t in latest $GIT_TAG; do
-    docker push "${REPO}:${t}"
-done
+docker buildx build -t $REPO:latest -t $REPO:$GIT_TAG --push --platform=linux/arm64,linux/amd64 -f Dockerfile .
